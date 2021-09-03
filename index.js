9c0a3db941b53748ee6ebf21caef985481ef8c48
Sacha Durand <sacha6623@gmail.com>
2021-09-03 16:32:30 +0200

    feat(selenium): update scrap function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const app = express();
 const port = process.env.PORT || 3456;
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 // options : https://stackoverflow.com/questions/63466426/handshake-failed-returned-1-ssl-error-code-1-net-error-201 
 
@@ -15,13 +16,11 @@ import chrome from 'selenium-webdriver/chrome.js';
 //firefox = require('selenium-webdriver/firefox');
 import 'chromedriver';
 
-
 import getEmailFromWebsite from './utils/utils.js';
 
 async function getCompanies(url){
 		
 		let companyNumber = 0;
-		let mail = '';
 
 		//.setPageLoadStrategy(PageLoadStrategy.NORMAL)   // pass to eager
 		let driver = chrome.Driver.createSession(new chrome.Options().addArguments(['--no-sandbox', /* '--headless', */  '--disable-dev-shm-usage', '--disable-gpu', '--disable-extensions',
@@ -31,24 +30,25 @@ async function getCompanies(url){
 
 		//driver.manage().waitForPageToLoad(30000);
 		driver.manage().timeouts().implicitlyWait(25);
-		driver.manage().window().setSize(993, 745); // it's a size of the browser window with full screen and open developper tools in chrome
+		//driver.manage().window().setSize(993, 745); // it's a size of the browser window with full screen and open developper tools in chrome
+		driver.manage().window().maximize();
 
 		let companies = [];
 		await driver.get(url);
 		driver.sleep(1000);
 		await driver.findElement(By.xpath('/html/body/c-wiz/div/div/div/div[2]/div[1]/div[4]/form/div[1]/div/button/span')).click(); // pass RGPD page
 
-		driver.sleep(1700);
-		let maxResultForPage = await driver.findElement(By.xpath('/html/body/jsl/div[3]/div[10]/div[8]/div/div[1]/div/div/div[4]/div[2]/div/div[1]/span/span[2]')).getText(); // 20 ? 
+		driver.sleep(3800);
+		let maxResultForPage = await driver.findElement(By.xpath('/html/body/jsl/div[3]/div[10]/div[8]/div/div[1]/div/div/div[4]/div[2]/div/div[1]/span/span[2]')).getText();
 		console.log(maxResultForPage);
 
-		if(maxResultForPage > 0){
+		if(Number(maxResultForPage) > 0){
 			for (let currentCompany = 1; currentCompany < maxResultForPage * 2 ; currentCompany = currentCompany = currentCompany + 2) {
 				driver.sleep(1600);
 
 				// SCROLL 
 				console.log('scroll');
-				let scrollValue = 900;
+				let scrollValue = 850;
 				//if(currentCompany > 5){ scrollValue = 1200;}
 				if(currentCompany > 7){ scrollValue = 1300;}
 				if(currentCompany > 9){scrollValue = 2000;}
@@ -57,43 +57,41 @@ async function getCompanies(url){
 				driver.executeScript("var el = document.evaluate('/html/body/jsl/div[3]/div[10]/div[8]/div/div[1]/div/div/div[4]/div[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; el.scroll(0, "+scrollValue+");");
 				
 				// SELECT COMPANY IN LIST
-				driver.sleep(1900);
+				driver.sleep(800);
 				console.log('selection element');
 				await driver.findElement(By.xpath('/html/body/jsl/div[3]/div[10]/div[8]/div/div[1]/div/div/div[4]/div[1]/div['+currentCompany+']/div/a')).click(); // select company
-				driver.sleep(1400);
-
+				driver.sleep(2400);
 
 				// GET ELEMENTS ON COMPANY PAGE
 
 				console.log('GET company');
 				var companyTitle =  await driver.findElement(By.xpath("//div[@role='main' and @aria-label]"))//.get_attribute('aria-label')
 					.then(function(element) { return element.getAttribute('aria-label'); })
-					.catch( function(err) {  console.log('error : '+err); });
+					.catch( function(err) {  });
 
 				var phone =  await driver.findElement(By.xpath("//button[contains(@aria-label, 'Numéro de téléphone:')]"))//.getAttribute('aria-label');
 					.then(function(element) { return element.getAttribute('aria-label'); })
-					.catch( function(err) {  console.log('error : '+err); });
+					.catch( function(err) {  });
 				var website =  await driver.findElement(By.xpath("//button[contains(@aria-label, 'Site Web:')]"))//.getAttribute('aria-label');
 					.then(function(element) { return element.getAttribute('aria-label'); })
-					.catch( function(err) {  console.log('error : '+err); });
+					.catch( function(err) {  });
 
 				var adresse =  await driver.findElement(By.xpath("//button[@data-item-id='address']"))//.getAttribute('aria-label');
 					.then(function(element) { return element.getAttribute('aria-label'); })
-					.catch( function(err) {  console.log('error : '+err); });
+					.catch( function(err) {  });
 
 				//FIND EMAIL
-				
-				try{
-					if(website != null && website != undefined){
-						website =	website.replace('Site Web: ','');
-						website =	website.replace(' ','');
-						mail = await getEmailFromWebsite('http://'+website);
-					}
-				}catch(err){}
+				if(website !== null && website !== undefined){
+					website =	website.replace('Site Web: ','');
+					website =	website.replace(' ','');
+					console.log(website);
+					var mail = await getEmailFromWebsite('https://'+website);
+					console.log(mail);
+				}
 				
 				//mail = 'TODO: True email from website';
 
-				driver.sleep(1000);
+				driver.sleep(2000);
 				//List elementsList = driver.findElements(By.xpath("//[contains(text(),'Selenium')]"));
 
 				console.log('construct object');
@@ -113,10 +111,18 @@ async function getCompanies(url){
 				console.log(companyNumber);
 				
 
-				driver.sleep(400);
+				driver.sleep(800);
 					
+				//REMOVE MENU IF IT ENABLED
+				/*
+				if(await driver.findElement(By.xpath('/html/body/jsl/div[3]/div[10]/div[24]/div/div[2]/ul/jsl[3]/ul[1]/li[1]/button/label')).isDisplayed() == true ){
+					await driver.findElement(By.xpath('/html/body/jsl/div[3]/div[10]/div[24]/div/div[2]/ul/div[2]/button')).click();
+				}
+				*/
+
 				console.log('Navigation Back');
-				await driver.findElement(By.xpath('/html/body/jsl/div[3]/div[10]/div[3]/div[1]/div[1]/div[1]/div[1]/button')).click();
+				//await driver.findElement(By.xpath('/html/body/jsl/div[3]/div[10]/div[3]/div[1]/div[1]/div[1]/div[1]/button')).click(); // work
+				await driver.findElement(By.xpath('/html/body/jsl/div[3]/div[10]/div[3]/div[1]/div[1]/div[1]/div[4]/div/div[1]/div/div/button')).click();
 				driver.sleep(1200);
 
 				//RESULT HANDLER
--- utils/utils.js
+++ utils/utils.js
@@ -1,12 +1,28 @@
 import fetch from "node-fetch";
 
- export default function getEmailFromWebsite(url){
-  console.log('scraping email in url...');
-  fetch(url).then(function(response) {
-    response.text().then(function(text) {
-      return text;
-    });
-  });
-}
+ export default async function getEmailFromWebsite(url){
+  let emailRegex = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[/fr|com|org|net|us|info/]+)/;
+  let emailRegex2 = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+  try{
+    return await fetch(url)
+      .then( res => { 
+        let htmlPage = res.text();     // res.text work ! 
+
+        // scraping email logic is good
+        console.log('scraping email in url...');
+        var mail = emailRegex2.exec(htmlPage);
+        if(mail){return mail[0]}
+        var mail = htmlPage.toString().match(emailRegex);
+        if(mail){return mail[0]}
+        var mail = htmlPage.toString().match(emailRegex2);
+        if(mail){return mail[0]}
+        return 'no email';
+      })
+      
+  }
+  catch{ return; }
+  
+    
+};
 
